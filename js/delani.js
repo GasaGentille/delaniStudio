a188017dec2c56ec52638bb4ea9fd0d8d5cb5ae3
Gentille <gisagentille55@gmail.com>
2019-07-26 17:33:42 +0200

    Done with toggle to what we do section

--- js/delani.js
+++ js/delani.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
     $("form").submit(function(event) {
         var inputData =$("input#nameInput").val();
+        var inputEmail=$("input#emailInput").val();
         var textareaData= $("textarea#textareaInput").val();
        
-        alert("Dear" +inputData+ "we have");
+        alert("Dear " +inputData+" we have received your message and thank you for contacting us.");
         });
-
-
-
+        $(".icons").click(function(){
+            $(".description").toggle();
+            $(".icons").toggle();
+        });
+        $(".description").click(function(){
+            $(".description").toggle();
+            $(".icons").toggle();
+        }); 
 });
-var inputData ;
-var textareaData;
+
