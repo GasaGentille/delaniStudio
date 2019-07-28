6a84e959e4e9ab8d638bb5eb49e455407ae2b7b3
Gentille <gisagentille55@gmail.com>
2019-07-28 16:47:44 +0200

    Give icons descriptions a bold style and putting it in center

--- js/delani.js
+++ js/delani.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
     $("form").submit(function(event) {
+        event.preventDefault()
         var inputData =$("input#nameInput").val();
         var inputEmail=$("input#emailInput").val();
-        var textareaData= $("textarea#textareaInput").val();
+        var textAreaData= $("textarea#textareaInput").val();
        
+        $("#form")[0].reset();
         alert("Dear " +inputData+" we have received your message and thank you for contacting us.");
+      
         });
-        $(".icons").click(function(){
-            $(".description").toggle();
-            $(".icons").toggle();
-        });
-        $(".description").click(function(){
-            $(".description").toggle();
-            $(".icons").toggle();
-        }); 
-});
+        var icn = $(".icons");
+        $.each(icn, function(index, element){
+                $(element).click(function(){
+                $(".description"+(index+1)).toggle();
+                $(this).toggle();
+            })
+            $(".description"+(index+1)).click(function(){
+                $(this).toggle();
+                $(element).toggle();
 
+            } );
+        });
+});
