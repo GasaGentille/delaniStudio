fe798add5cffcf95054829ac39b275a9d2030957
Gentille <gisagentille55@gmail.com>
2019-07-28 21:13:12 +0200

    form validation

--- js/delani.js
+++ js/delani.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
+
     $("form").submit(function(event) {
         event.preventDefault()
         var inputData =$("input#nameInput").val();
         var inputEmail=$("input#emailInput").val();
         var textAreaData= $("textarea#textareaInput").val();
-       
-        $("#form")[0].reset();
-        alert("Dear " +inputData+" we have received your message and thank you for contacting us.");
-      
+
+        $(".error").remove();
+        
+        if (inputData.length < 1) {
+            $('#nameInput').after('<span class="error">This field is required</span>');
+        }
+        else if (inputEmail.length < 1) {
+            $('#emailInput').after('<span class="error">This field is required</span>');
+            
+        }else{
+            var regEx = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+            var validEmail = regEx.test(inputEmail);
+            if (!validEmail) {
+                $('#emailInput').after('<span class="error">Enter a valid email</span>');
+            }else{
+                if (textAreaData.length < 1) {
+                    $('#textareaInput').after('<span class="error">This field is required</span>');
+                }  else {
+            
+                        $("#form")[0].reset();
+                        alert("Dear " +inputData+" we have received your message and thank you for contacting us.");
+                    }
+                }
+            }
         });
+        
         var icn = $(".icons");
         $.each(icn, function(index, element){
                 $(element).click(function(){
@@ -21,4 +43,5 @@ $(document).ready(function() {
 
             } );
         });
+        
 });
